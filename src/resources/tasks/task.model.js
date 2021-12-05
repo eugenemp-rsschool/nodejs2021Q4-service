e0faa04ef94d6d1913750faa70056a4e02f96267
eugenemp <phoenixlp91@mail.ru>
2021-12-05 23:29:15 +0300

    feat: implement task model

---
+++ src/resources/tasks/task.model.js
@@ -0,0 +1,43 @@
+const { v4: uuidv4 } = require('uuid');
+
+class Task {
+  constructor({
+    id = uuidv4(),
+    title = 'TASK',
+    order = 'task',
+    description = 'desc',
+    userId = null,
+    boardId = null,
+    columnId = null,
+  } = {}) {
+    this.id = id;
+    this.title = title;
+    this.order = order;
+    this.description = description;
+    this.userId = userId;
+    this.boardId = boardId;
+    this.columnId = columnId;
+  }
+
+  static validateTask(task) {
+    const props = [
+      'title',
+      'order',
+      'description',
+      'userId',
+      'boardId',
+      'columnId',
+    ];
+    if (!props.every((prop) => prop in task)) return false;
+
+    /* const typeTest = Object.keys(task).every(
+      (key) => props.includeskey && (typeof key === 'string' || 'null')
+    );
+
+    if (!typeTest) return false; */
+
+    return true;
+  }
+}
+
+module.exports = Task;
